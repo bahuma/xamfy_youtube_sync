3c9da3ff717d4fc1ae0ae0343e5cb39845c7fa5b
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2014-08-18 21:39:16 +0000

    Fixed typo

--- sync.php
+++ sync.php
@@ -42,7 +42,7 @@ if ($client->getAccessToken()) {
           
           $xamfy_metadata = json_decode(file_get_contents($config['xamfy_api']['url']."/".$nid));
           
-          if (count($xamfy_metatada > 0))
+          if (count($xamfy_metadata > 0))
             print_r($xamfy_metadata);
           
       }
